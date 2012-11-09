166165a05bc773a54125f8d3beb2bfaf6a3f54ce
Rodin <schludern@gmail.com>
2012-11-09 14:25:19 +0800

    add rodin.sh

--- src/etc/bashrc.d/rodin.sh
+++ src/etc/bashrc.d/rodin.sh
@@ -4,11 +4,6 @@ export GOPATH=/home/rodin/sandbox/go/3rdparty:/home/rodin/sandbox/go/own
 #export GOBIN=/home/rodin/sandbox/go/go/bin
 export PATH=/home/rodin/bin:$PATH:$GOROOT/bin:$GOBIN
 
-# Alias definitions.
-# You may want to put all your additions into a separate file like
-# ~/.bash_aliases, instead of adding them here directly.
-# See /usr/share/doc/bash-doc/examples in the bash-doc package.
-
 if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
 fi
