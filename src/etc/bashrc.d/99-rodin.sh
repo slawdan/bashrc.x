65c3252118ff6784c5db77a11598a0a469f42a15
Rodin <schludern@gmail.com>
2012-12-04 11:21:50 +0800

    merge my file

---
+++ src/etc/bashrc.d/99-rodin.sh
@@ -0,0 +1,12 @@
+export SCALA_HOME=/home/rodin/sandbox/scala/scala/
+export GOROOT=/home/rodin/sandbox/go/go
+export GOPATH=/home/rodin/sandbox/go/3rdparty:/home/rodin/sandbox/go/own
+#export GOBIN=/home/rodin/sandbox/go/go/bin
+export PATH=/home/rodin/bin:$PATH:$GOROOT/bin:$GOBIN
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+
+
